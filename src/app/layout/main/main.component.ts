dea09f1ea6c5ef9fb89585522ca1b4928e796a3a
Afiqah Malic <nurafiqahmalic@gmail.com>
2024-08-30 16:43:25 +0800

    Remove Payment and Account Settings

--- src/app/layout/main/main.component.ts
+++ src/app/layout/main/main.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrl: './main.component.scss'
+  styleUrls: ['./main.component.scss'],
 })
-export class MainComponent {
+export class MainComponent implements OnInit {
+  currentDateTime: string = ''; // Initialize with an empty string
 
+  ngOnInit() {
+    this.updateDateTime();
+    setInterval(() => this.updateDateTime(), 1000); // Update every second
+  }
+
+  updateDateTime() {
+    const now = new Date();
+    this.currentDateTime = now.toLocaleString(); // Format as needed
+  }
 }
